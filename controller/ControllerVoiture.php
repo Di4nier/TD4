aa2d5046a6cf60aa18fc2ebfdbf344c845080b97
TheAdmin <didibleus2002@gmail.com>
2022-10-10 15:57:00 +0200

    Exercice 4 Fini

--- controller/ControllerVoiture.php
+++ controller/ControllerVoiture.php
@@ -9,10 +9,13 @@
 
     <?php
     require_once '../model/ModelVoiture.php'; // chargement du modèle
-    $tab_v = ModelVoiture::getAllVoitures(); //appel au modèle pour gerer la BD
-    require '../view/voiture/list.php'; //redirige vers la vue
-
-?>
+    class ControllerVoiture{
+        public static function readAll(){
+            $tab_v = ModelVoiture::getAllVoitures(); //appel au modèle pour gerer la BD
+            require '../view/voiture/list.php'; //redirige vers la vue
+        }
+    }
+    ?>
 
 </body>
 </html>
